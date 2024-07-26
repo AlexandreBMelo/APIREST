b0b485c8e6ca27448d96321c3eedabdb54aec555
AlexandreMelo <alemelo18lds@gmail.com>
2024-07-26 20:08:15 -0300

    Commit 05 - hotfix precision/scale

---
+++ src/main/java/dev/alexandre/apirest/domain/model/Account.java
@@ -0,0 +1,62 @@
+package dev.alexandre.apirest.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+
+public class Account {
+    //     Atributos e Mapeamento de Entidades
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(precision = 13, scale = 2)
+    private BigDecimal balance;
+
+    @Column(name = "additional_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/main/java/dev/alexandre/apirest/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package dev.alexandre.apirest.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
